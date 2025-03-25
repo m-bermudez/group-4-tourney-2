b0145fff23392b99d3ff734b342f8f900e287a31
Ryan <stopczynski@usf.edu>
2025-03-25 10:03:06 -0400

    Created Person.cs with main class.

---
+++ Tourney 2/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tourney_2
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string ID { get; set; }
+
+        public Person(string name, string email, string id)
+        {
+            Name = name;
+            Email = email;
+            ID = id;
+        }
+        public virtual void DisplayInfo()
+        {
+            Console.WriteLine($"Name: {Name}, Email: {Email}, ID: {ID}");
+        }
+    }
+}
